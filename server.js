37902f4e404a08dd212fb0f7c3a56fb5493d4ff7
Ayana Griffin <ayanajg@gmail.com>
2021-03-03 12:01:20 -0800

    changing db rqs

--- server.js
+++ server.js
@@ -8,10 +8,8 @@ const knex = require("knex");
 const db = knex({
   client: "pg",
   connection: {
-    host: "127.0.0.1", //localhost
-    user: "ayana",
-    password: "",
-    database: "smart-brain",
+    host: process.env.DATABASE_URL,
+    ssl: true,
   },
 });
 
