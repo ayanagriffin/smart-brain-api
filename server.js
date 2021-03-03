eae0c104e1b42596cb6d28cc470c95539d8cd5cb
Ayana Griffin <ayanajg@gmail.com>
2021-03-03 13:13:38 -0800

    debugging

--- server.js
+++ server.js
@@ -30,13 +30,13 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  db.select('*').from('users').then(users => {
-    users.json()
-  }).then(users => {
-    res.send("testing");
-  })
+  // db.select('*').from('users').then(users => {
+  //   users.json()
+  // }).then(users => {
+  //   res.send("testing");
+  // })
 
-  // res.send('keep trying :)')
+  res.send('keep trying :)')
   
 });
 
