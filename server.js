182655ffffdac8d49d5351b087ee3689a9bf2a08
Ayana Griffin <ayanajg@gmail.com>
2021-03-03 12:54:05 -0800

    debugging

--- server.js
+++ server.js
@@ -40,37 +40,37 @@ app.get("/", (req, res) => {
   
 });
 
-app.post("/signin", (req, res) => {
-  signin.handleSignin(req, res, db, bcrypt);
-});
+// app.post("/signin", (req, res) => {
+//   signin.handleSignin(req, res, db, bcrypt);
+// });
 
-app.post("/register", (req, res) => {
-  register.handleRegister(req, res, db, bcrypt);
-});
+// app.post("/register", (req, res) => {
+//   register.handleRegister(req, res, db, bcrypt);
+// });
 
-app.get("/user/:id", (req, res) => {
-  user.handleGetUser(req, res, db);
-});
+// app.get("/user/:id", (req, res) => {
+//   user.handleGetUser(req, res, db);
+// });
 
-app.get("/rank/:id", (req, res) => {
-  rank.handleRank(req, res, db);
-});
+// app.get("/rank/:id", (req, res) => {
+//   rank.handleRank(req, res, db);
+// });
 
-app.put("/image", (req, res) => {
-  image.handleImage(req, res, db);
-});
+// app.put("/image", (req, res) => {
+//   image.handleImage(req, res, db);
+// });
 
-app.post("/imageurl", (req, res) => {
-  image.handleApiCall(req, res);
-});
+// app.post("/imageurl", (req, res) => {
+//   image.handleApiCall(req, res);
+// });
 
-app.get("/leaderboard", (req, res) => {
-  leaderboard.handleLeaderboard(req, res, db);
-});
+// app.get("/leaderboard", (req, res) => {
+//   leaderboard.handleLeaderboard(req, res, db);
+// });
 
-app.delete("/user/:id", (req, res) => {
-  user.handleDeleteUser(req, res, db);
-});
+// app.delete("/user/:id", (req, res) => {
+//   user.handleDeleteUser(req, res, db);
+// });
 
 app.listen(process.env.PORT || 3000, () => {
   console.log(`app is running on port ${process.env.PORT}`);
